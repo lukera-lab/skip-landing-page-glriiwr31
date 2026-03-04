595971f768cc9f61a82b86cd8880d777259d3372
Skip <skip@usecurling.com>
2026-03-04 22:27:56 +0000

    Set custom variable font as global default.

--- dev-dist/assets/index-DuxGfwzc.js
+++ dev-dist/assets/index-DuxGfwzc.js
@@ -24036,4 +24036,4 @@ var App = () => /* @__PURE__ */ (0, import_jsx_runtime.jsx)(BrowserRouter, {
 var App_default = App;
 (0, import_client.createRoot)(document.getElementById("root")).render(/* @__PURE__ */ (0, import_jsx_runtime.jsx)(App_default, {}));
 
-//# sourceMappingURL=index-QJj3ttCF.js.map
+//# sourceMappingURL=index-DuxGfwzc.js.map
